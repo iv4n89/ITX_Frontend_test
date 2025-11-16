80e4badf226eca74f060cc76ddfdb8658ab2040a
iv4n89 <ivan.betanzos89@gmail.com>
2025-11-17 00:59:32 +0100

    feat: product details components

---
+++ src/ui/itx-store/components/product/product.jsx
@@ -0,0 +1,20 @@
+import { ProductSpecs } from '../product-specs/product-specs';
+import './product.css';
+
+export const Product = ({ data }) => {
+  return (
+    <div className="product_info__container">
+      <div className="product_info__header">
+        <h1 className="product_info__brand">{data.brand}</h1>
+        <h2 className="product_info__model">{data.model}</h2>
+        <p className="product_info__price">
+          {data.price.length ? data.price : '-'} €
+        </p>
+      </div>
+
+      <div className="product_info__specs_container">
+        <ProductSpecs data={data} />
+      </div>
+    </div>
+  );
+};
