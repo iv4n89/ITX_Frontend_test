c9100ae057591df0818d7e19a3adaeab84c8f3b0
iv4n89 <ivan.betanzos89@gmail.com>
2025-11-16 01:37:05 +0100

    feat: add layout component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import './App.css';
 import { QueryProviderClient } from '@/ui/common/util/query-client/query-client-provider';
 import { Routes } from '@/ui/common/routes/routes';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { CartProvider } from './ui/common/context/cart-context/cart-context-provider';
 
 function App() {
   return (
     <>
       <QueryProviderClient>
-        <RouterProvider router={Routes} />
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          buttonPosition="bottom-right"
-        />
+        <CartProvider>
+          <RouterProvider router={Routes} />
+          <ReactQueryDevtools
+            initialIsOpen={false}
+            buttonPosition="bottom-right"
+          />
+        </CartProvider>
       </QueryProviderClient>
     </>
   );
---
+++ src/ui/common/context/cart-context/use-cart-context.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CartContext } from './cart-context';
+
+export const useCart = () => {
+  const context = React.useContext(CartContext);
+
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+
+  return context;
+};
