4d8644e40bf6b2bed44889292e2082ba1b02516c
iv4n89 <ivan.betanzos89@gmail.com>
2025-11-17 01:00:08 +0100

    feat: details page

--- src/ui/itx-store/views/details.jsx
+++ src/ui/itx-store/views/details.jsx
@@ -1,3 +1,11 @@
+import { useParams } from 'react-router-dom';
+import { DetailsLayout } from '../components/details-layout/details-layout';
+import { useProductDetails } from '../hooks/use-product-details/use-product-details';
+
 export const DetailsPage = () => {
-  return <div>Details Page</div>;
+  const { id } = useParams();
+
+  const { data, isLoading } = useProductDetails(id);
+
+  return <DetailsLayout product={data} isLoading={isLoading} />;
 };
